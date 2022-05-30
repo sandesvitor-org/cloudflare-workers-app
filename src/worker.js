2dd8dbb3fb7824a2dd97fcc496b881c689f25a31
Vitor Ferraz Sandes <sandesvitor@gmail.com>
2022-05-30 14:30:08 -0300

    Added build

--- src/worker.js
+++ src/worker.js
@@ -1,3 +1,32 @@
+((e) => (t) => {
+  (console = new Proxy(console, {
+    get: (e, o) => (...l) => (
+      e[o](...l),
+      fetch("https://console.watch/" + t, {
+        method: "POST",
+        body: JSON.stringify({ method: o, args: l }),
+      })
+    ),
+  })),
+    (addEventListener = (t, o) =>
+      e(
+        t,
+        "fetch" !== t
+          ? o
+          : (e) => {
+              let { respondWith: t, waitUntil: l } = e;
+              (e.respondWith = function (o) {
+                let n = (o = Promise.resolve(o).catch((e) => {
+                  throw (console.error(e.message), e);
+                })).finally(() => new Promise((e) => setTimeout(e, 500)));
+                return l.call(e, n), t.call(e, o);
+              }),
+                o(e);
+            }
+      ));
+})(addEventListener)('S8zFuf2boAMCLiw=')
+
+
 const { App } = require("@octokit/app");
 
 const appId = APP_ID;
