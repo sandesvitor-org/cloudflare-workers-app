a29da6302c6448e1f94332a03ac114d21d6ad22e
Vitor Ferraz Sandes <sandesvitor@gmail.com>
2022-05-30 11:54:11 -0300

    Added build

--- src/worker.js
+++ src/worker.js
@@ -1,5 +1,4 @@
 const { App } = require("@octokit/app");
-// const { handleBadDatabaseVerbs, handleTest } = require("./handlers/handle_bad_verbs");
 
 const appId = APP_ID;
 const secret = WEBHOOK_SECRET;
@@ -19,8 +18,7 @@ const app = new App({
 });
 
 app.webhooks.on(PR_EVENTS, async ({ octokit, payload }) => {
-  // await handleBadDatabaseVerbs(octokit, payload, APP_NAME, BAD_VERBS, TEAM_REVIEWERS);
-  await handleTest(octokit, payload);
+  await handleBadDatabaseVerbs(octokit, payload, APP_NAME, BAD_VERBS, TEAM_REVIEWERS);
 });
 
 addEventListener("fetch", (event) => {
@@ -72,17 +70,69 @@ async function handleRequest(request) {
   }
 }
 
+
+/* 
+ * 
+ * IMPLEMENTING CUSTOM HANDLERS
+ * 
+*/
+async function handleBadDatabaseVerbs(octokit, payload, appName, badVerbs, teamReviewrs){
+  const commit_id = payload.pull_request.head.sha;
+  const owner = payload.repository.owner.login;
+  const repo = payload.repository.name;
+  const pull_number = payload.number;
+  const ref = payload.pull_request.head.ref;
+  // const prURL = payload.pull_request.html_url;
+  // const prAuthor = payload.pull_request.user.login;
+
+  const botPullRequestReviewsIDsArray = await getPullRequestReviews(octokit, {owner, repo, pull_number, app_name: appName});
+  const filesContentArray = await getPullRequestChangedFilesContent(octokit, {owner, repo, pull_number, ref});
+  
+  filesContentArray.forEach(async (file) => {
+    const openReviewsForFile = botPullRequestReviewsIDsArray.filter(review => review.file_path == file.name && review.state !== 'DISMISSED')
+    
+    // Checking with there is any naughty verb in PR changed files:
+    if (badVerbs.some(verb => file.content.includes(verb)))
+    {
+      // Checking if we already have a review in PR linked to the file name (also, if said review is marked as 'DISMISSED', return check):
+      if (openReviewsForFile.length > 0){
+        console.info(`Ignoring file [${file.name}] because a review is already set for it`)
+        return
+      } 
+
+      // If there is no review AND the file has some BAD VERBS, create a review:
+      await postReviewCommentInPullRequest(octokit, {owner, repo, pull_number, commit_id, path: file.name});
+      await requestReviewerForPullRequest(octokit, {owner, repo, pull_number, team_reviewers: teamReviewrs});
+      console.info(`Creating a review for file [${file.name}] due to forbidden verbs: [${badVerbs}]`);
+    } 
+    else 
+    {
+      openReviewsForFile.forEach(review => {
+          console.info(`Dismissing review [${review.review_id}] for file [${file.name}]`);
+          await dismissReviewForPR(octokit, {owner, repo, pull_number, review_id: review.review_id});
+        });
+      console.info(`Ignoring changed file [${file.name}], nothing wrong with it =)`);
+    }
+  })
+}
+
+
+/* 
+ * 
+ * GITHUB API FUNCTIONS
+ * 
+*/
 async function getPullRequestChangedFilesContent(octokit, {owner, repo, pull_number, ref}){
   let filesContent = []
-  const filesListBase64 = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}/files', {
+  const filesListBase64 = await octokit.pulls.listFiles({
     owner,
     repo,
     pull_number,
     per_page: 100
   }).then(filesObject => filesObject.data)
-
+  
   for(let i =0; i < filesListBase64.length; i++){
-    let content = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}',{
+    let content = await octokit.repos.getContent({
       owner: owner,
       repo: repo,
       path: filesListBase64[i].filename,
@@ -98,22 +148,28 @@ async function getPullRequestChangedFilesContent(octokit, {owner, repo, pull_num
 
   return filesContent;
 }
-
-async function handleTest(octokit, payload){
-  const commit_id = payload.pull_request.head.sha;
-  const owner = payload.repository.owner.login;
-  const repo = payload.repository.name;
-  const pull_number = payload.number;
-  const ref = payload.pull_request.head.ref;
-  const base = payload.pull_request.base.ref; 
-
-  const filesContentArray = await getPullRequestChangedFilesContent(octokit, {owner, repo, pull_number, ref});
-
-  await postReviewCommentInPullRequest(octokit, {owner, repo, pull_number, commit_id, path: filesContentArray[0].name})
+  
+async function getPullRequestReviews(octokit, {owner, repo, pull_number, app_name}){
+  return await octokit.pulls.listReviews({
+      owner,
+      repo,
+      pull_number,
+  }).then(res => res.data.filter(review => review.user.login == app_name).map(data => { 
+    return {review_id: data.id, file_path: data.body, state: data.state} 
+  }));
 }
-
+  
+async function requestReviewerForPullRequest(octokit, {owner, repo, pull_number, team_reviewers}){
+  await octokit.pulls.requestReviewers({
+    owner,
+    repo,
+    pull_number,
+    reviewers: team_reviewers
+  })
+}
+  
 async function postReviewCommentInPullRequest(octokit, {owner, repo, pull_number, commit_id, path}){
-  await octokit.request('POST /repos/{owner}/{repo}/pulls/{pull_number}/reviews', {
+  await octokit.pulls.createReview({
     owner: owner,
     repo: repo,
     pull_number: pull_number,
@@ -130,5 +186,15 @@ async function postReviewCommentInPullRequest(octokit, {owner, repo, pull_number
         body: `File ${path} have dangerous query verbs!`
       }
     ]
+  });
+}
+
+async function dismissReviewForPR(octokit, {owner, repo, pull_number, review_id}){
+  octokit.pulls.dismissReview({
+    owner,
+    repo,
+    pull_number,
+    review_id,
+    message: "Dismissing review due to resolved BAD VERBS"
   });
 }
